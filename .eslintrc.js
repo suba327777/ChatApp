8cb0626195e12f63cbb4bbd565dd0fdfad283b8e
suba327777 <suba.1357@ezweb.ne.jp>
2022-03-29 17:39:41 +0900

    react typescript ruleの追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["plugin:react/recommended", "airbnb", "airbnb-typescript", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaFeatures: {
       jsx: true,
     },
@@ -14,5 +15,16 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint"],
   ignorePatterns: [".eslintrc.js"],
-  rules: {},
+  rules: {
+    "import/no-unresolved": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+    "import/prefer-default-export": 0,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "function-expression",
+      },
+    ],
+  },
 };
