2ac0e5e8d0a94884807160027a0a5db86376d571
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 18:53:43 +0900

    Provider設定

--- index.tsx
+++ index.tsx
@@ -1,6 +1,15 @@
+/* packages */
 import React from "react";
 import ReactDOM from "react-dom";
-
+/* redux */
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+/* App */
 import { App } from "./src/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
