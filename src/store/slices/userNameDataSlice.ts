15af784a0919ba5cd6a3080a4ba835eb6f473481
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 21:25:26 +0900

    first commit

---
+++ src/store/slices/userNameDataSlice.ts
@@ -0,0 +1,28 @@
+/* packages */
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+/* hooks */
+import { useAppSelector } from "../../hooks/useAppselector";
+/* types */
+import type { RootState } from "../types/reduxType";
+
+const initialState: string = "";
+
+export const userNameDataSlice = createSlice({
+  name: "userNameData",
+  initialState,
+  reducers: {
+    setUserNameData: (state, action: PayloadAction<string>) => action.payload,
+  },
+});
+
+// reducer
+export const userNameDataReducer = userNameDataSlice.reducer;
+
+// action
+export const { setUserNameData } = userNameDataSlice.actions;
+
+// selector
+export const useUserNameDataSelector = () => {
+  const userNameSelector = useAppSelector((state: RootState) => state.userNameData);
+  return userNameSelector;
+};
