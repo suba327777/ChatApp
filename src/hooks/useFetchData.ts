fef00bf077abeeadc755eddf68956ae99502a34a
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 18:56:23 +0900

    redux処理の追加

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,21 +1,25 @@
 /* packages */
-import { useEffect, useState } from "react";
-/* firebase */
+import { useEffect } from "react";
+/* hooks */
+import { useAppDispatch } from "./useAppDispatch";
+/* lib */
 import { chatRef } from "../lib/firebase";
+import { convertChatList } from "../lib/convertChatList";
+/* store */
+import { setChatData } from "../store/slices/chatDataSlice";
 
 export const useFetchData = () => {
-  const [data, setData] = useState();
-
+  const dispatch = useAppDispatch();
   useEffect(() => {
-    chatRef.on("value", (snapshot) => {
+    chatRef.on("value", async (snapshot) => {
       if (snapshot?.val()) {
-        setData(snapshot.val());
+        const chatData = await snapshot.val();
+        const convertChatData = convertChatList(chatData);
+        dispatch(setChatData(convertChatData));
       }
     });
     return () => {
       chatRef.off();
     };
-  }, [chatRef]);
-
-  return data;
+  }, []);
 };
