989262dc6f42a2452e83692c79701120feefd3e2
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 21:25:58 +0900

    add userNameDataReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 /* slice */
 import { chatDataReducer } from "./slices/chatDataSlice";
+import { userNameDataReducer } from "./slices/userNameDataSlice";
 
 export const store = configureStore({
   reducer: {
     chatData: chatDataReducer,
+    userNameData: userNameDataReducer,
   },
 });
