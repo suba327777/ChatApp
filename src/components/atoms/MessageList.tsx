d2364c26c8bbb42fc55f98cc3d2541d48484850c
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 21:26:55 +0900

    リファクタ

--- src/components/atoms/MessageList.tsx
+++ src/components/atoms/MessageList.tsx
@@ -3,19 +3,33 @@ import React from "react";
 import { Link } from "react-scroll";
 /* store */
 import { useChatDataSelector } from "../../store/slices/chatDataSlice";
+import { useUserNameDataSelector } from "../../store/slices/userNameDataSlice";
 
 export const MessageList: React.VFC = () => {
   const chatData = useChatDataSelector();
+  const userNameData = useUserNameDataSelector();
   return (
     <div>
       <Link to="concept" smooth duration={600} offset={-50}>
         {chatData.length === 0 ? (
           // ローディング処理を入れる
-          <span>チャットはないです</span>
+          <span>ローディング中です</span>
         ) : (
           chatData.map((item) => (
             <div key={item.key}>
-              <p>{item.text}</p>
+              <p>
+                {/* 名前を入力したらreduxで名前を格納してその名前を元に */}
+                {/* 送られたメッセージが自分ではない場合は右に送信する */}
+                {item.userName === userNameData ? (
+                  <div>
+                    {`${item.userName}`} {item.text}
+                  </div>
+                ) : (
+                  <div>
+                    {`${item.userName}違う人`} {item.text}
+                  </div>
+                )}
+              </p>
             </div>
           ))
         )}
