0e32d064f3a95616aa1f2e17477de42b947560fa
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-05 21:27:07 +0900

    リファクタ

--- src/components/atoms/TextInput.tsx
+++ src/components/atoms/TextInput.tsx
@@ -1,11 +1,11 @@
 import React, { ChangeEvent } from "react";
-import { TextField } from "@mui/material";
+import { Input } from "@mui/material";
 
 type Props = {
-  label: string;
+  placeholder: string;
   value: string | number;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
-export const TextInput: React.VFC<Props> = ({ label, value, onChange }) => (
-  <TextField label={label} value={value} onChange={onChange} />
+export const TextInput: React.VFC<Props> = ({ placeholder, value, onChange }) => (
+  <Input placeholder={placeholder} value={value} onChange={onChange} />
 );
