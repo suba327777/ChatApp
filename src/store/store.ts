15d606ed66fc29fe65b5aac2a21a3cd47b0b1b09
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-07 11:55:33 +0900

    add reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 /* slice */
 import { chatDataReducer } from "./slices/chatDataSlice";
-import { userNameDataReducer } from "./slices/userNameDataSlice";
+import { userNameDataReducer } from "./slices/user/userNameDataSlice";
+import { userIdDataReducer } from "./slices/user/userIdDataSlice";
+import { imageUrlDataReducer } from "./slices/imageUrlDataSlice";
 
 export const store = configureStore({
   reducer: {
     chatData: chatDataReducer,
     userNameData: userNameDataReducer,
+    userIdData: userIdDataReducer,
+    imageUrlData: imageUrlDataReducer,
   },
 });
