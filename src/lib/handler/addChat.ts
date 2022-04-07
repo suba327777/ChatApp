bcc4dfe2ddec05142c863cd0d012beff00d108f8
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-07 11:55:07 +0900

    add userId

--- src/lib/handler/addChat.ts
+++ src/lib/handler/addChat.ts
@@ -1,21 +1,23 @@
 /* packages */
 import moment from "moment";
 /* firebase */
-import { chatRef } from "../firebase";
+import { chatRef } from "../firebase/firebase";
 
 type Props = {
+  userId: string;
   userName: string;
   text: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export const addChat = async ({ userName, text, setText }: Props) => {
+export const addChat = async ({ userId, userName, text, setText }: Props) => {
   const createdAt = moment();
   const formatCreatedAt = createdAt.format("MM月DD日HH時mm分 (ddd)");
   // TODO文字列がない時はボタンを押せない様にする
 
   if (text.length === 0) return;
   await chatRef.push().set({
+    userId,
     userName,
     text,
     createdAt: formatCreatedAt,
