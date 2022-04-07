1111024c18a5a9af49b324f71f62d568fea709ca
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-07 21:16:51 +0900

    リファクタ

--- src/hooks/useFetchImage.ts
+++ src/hooks/useFetchImage.ts
@@ -1,15 +1,18 @@
+/* packages */
+import { useEffect } from "react";
 /* hooks */
 import { useAppDispatch } from "./useAppDispatch";
 /* firebase */
-import { useEffect } from "react";
 import { storageRef } from "../lib/firebase/firebase";
 /* store */
 import { setImageUrlData } from "../store/slices/imageUrlDataSlice";
+
 export const useFetchImage = () => {
   try {
     const dispatch = useAppDispatch();
+
     useEffect(() => {
-      async () => {
+      const fetchImageFn = async () => {
         const fetchAllImage = await storageRef.listAll();
         const N = Math.floor(Math.random() * fetchAllImage.items.length);
         const imagePath = fetchAllImage.items[N].fullPath;
@@ -17,6 +20,8 @@ export const useFetchImage = () => {
         const fetchImageUrl = await storageRef.child(imagePath).getDownloadURL();
         dispatch(setImageUrlData(fetchImageUrl));
       };
+
+      fetchImageFn();
     }, []);
   } catch (err: unknown) {
     console.log(err);
