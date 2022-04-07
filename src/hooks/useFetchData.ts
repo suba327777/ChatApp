e2b563f32fd0519d9cea332a694afc99c9707a08
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-07 11:55:48 +0900

    try catch処理の追加

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -3,23 +3,27 @@ import { useEffect } from "react";
 /* hooks */
 import { useAppDispatch } from "./useAppDispatch";
 /* lib */
-import { chatRef } from "../lib/firebase";
-import { convertChatList } from "../lib/convertChatList";
+import { chatRef } from "../lib/firebase/firebase";
+import { convertChatList } from "../lib/function/convertChatList";
 /* store */
 import { setChatData } from "../store/slices/chatDataSlice";
 
 export const useFetchData = () => {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    chatRef.on("value", async (snapshot) => {
-      if (snapshot?.val()) {
-        const chatData = await snapshot.val();
-        const convertChatData = convertChatList(chatData);
-        dispatch(setChatData(convertChatData));
-      }
-    });
-    return () => {
-      chatRef.off();
-    };
-  }, []);
+  try {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+      chatRef.on("value", async (snapshot) => {
+        if (snapshot?.val()) {
+          const chatData = await snapshot.val();
+          const convertChatData = convertChatList(chatData);
+          dispatch(setChatData(convertChatData));
+        }
+      });
+      return () => {
+        chatRef.off();
+      };
+    }, []);
+  } catch (err: unknown) {
+    console.log(err);
+  }
 };
