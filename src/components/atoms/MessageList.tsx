f789b27f894fb95ddd47155f15d75c2fea545811
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-07 11:50:45 +0900

    名前の認証の追加とデザイン

--- src/components/atoms/MessageList.tsx
+++ src/components/atoms/MessageList.tsx
@@ -1,39 +1,97 @@
 /* packages */
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-scroll";
 /* store */
 import { useChatDataSelector } from "../../store/slices/chatDataSlice";
-import { useUserNameDataSelector } from "../../store/slices/userNameDataSlice";
+import { useUserNameDataSelector } from "../../store/slices/user/userNameDataSlice";
+import { useUserIdDataSelector } from "../../store/slices/user/userIdDataSlice";
 
 export const MessageList: React.VFC = () => {
   const chatData = useChatDataSelector();
   const userNameData = useUserNameDataSelector();
+  const userIdData = useUserIdDataSelector();
   return (
-    <div>
-      <Link to="concept" smooth duration={600} offset={-50}>
-        {chatData.length === 0 ? (
-          // ローディング処理を入れる
-          <span>ローディング中です</span>
-        ) : (
-          chatData.map((item) => (
-            <div key={item.key}>
-              <p>
-                {/* 名前を入力したらreduxで名前を格納してその名前を元に */}
-                {/* 送られたメッセージが自分ではない場合は右に送信する */}
-                {item.userName === userNameData ? (
-                  <div>
-                    {`${item.userName}`} {item.text}
-                  </div>
-                ) : (
-                  <div>
-                    {`${item.userName}違う人`} {item.text}
-                  </div>
-                )}
-              </p>
-            </div>
-          ))
-        )}
-      </Link>
-    </div>
+    <SBody>
+      {/* <Link to="concept" smooth duration={600} offset={-50}> */}
+      {chatData.length === 0 ? (
+        // ローディング処理を入れる
+        <span>ローディング中です.....</span>
+      ) : (
+        chatData.map((item) => (
+          <div key={item.key}>
+            {/* 名前を入力したらreduxで名前を格納してその名前を元に */}
+            {/* 送られたメッセージが自分ではない場合は右に送信する */}
+            {item.userName === userNameData && item.userId === userIdData ? (
+              <SChatRight>
+                <SChatRightText>{item.text}</SChatRightText>
+                <p>{item.createdAt}</p>
+              </SChatRight>
+            ) : (
+              <div>
+                <div style={{ display: "flex" }}>
+                  <SChatLeftText>{item.text}</SChatLeftText>
+                  <p>{`${item.userName}`}</p>
+                </div>
+
+                <p>{item.createdAt}</p>
+              </div>
+            )}
+          </div>
+        ))
+      )}
+      {/* </Link> */}
+    </SBody>
   );
 };
+
+// const SChatContainer = styled.div``;
+const SBody = styled.div`
+  background-color: #93aad4;
+  margin: 0;
+  padding: 0;
+`;
+
+const SChatRight = styled.div`
+  text-align: end;
+`;
+
+const SChatRightText = styled.p`
+  position: relative;
+  display: inline-block;
+  margin: 1.5em 15px 1.5em 0;
+  padding: 7px 10px;
+  max-width: 100%;
+  color: #555;
+  font-size: 16px;
+  background: #85e14b;
+  border-radius: 15px;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: -24px;
+    margin-top: -12px;
+    border: 12px solid transparent;
+  }
+`;
+
+const SChatLeftText = styled.p`
+  position: relative;
+  display: inline-block;
+  margin: 1.5em 0 1.5em 15px;
+  padding: 7px 10px;
+  max-width: 100%;
+  color: #555;
+  font-size: 16px;
+  background: #fff;
+  border-radius: 15px;
+  &before: {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -24px;
+    margin-top: -12px;
+    border: 12px solid transparent;
+  }
+`;
