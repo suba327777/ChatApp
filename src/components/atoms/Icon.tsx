4e1b703e023f830c1f8f4f43b4c4bcc65098cfc2
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-08 20:39:09 +0900

    デザイン

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,22 +1,20 @@
 /* packages */
 import React from "react";
 import styled from "styled-components";
-/* slice */
-import { useImageUrlDataSelector } from "../../store/slices/imageUrlDataSlice";
 
 type Props = {
   photoUrl: string;
 };
-export const Icon: React.VFC<Props> = ({ photoUrl }) => {
-  return (
-    <SImageContainer>
-      <img src={photoUrl} alt="プロフィール画像" />
-    </SImageContainer>
-  );
-};
+export const Icon: React.VFC<Props> = ({ photoUrl }) => (
+  <div>
+    <SImage src={photoUrl} alt="プロフィール画像" />
+  </div>
+);
 
-const SImageContainer = styled.div`
+const SImage = styled.img`
+  width: 38px;
+  height: 38px;
+  object-fit: cover;
+  padding: 13px 2px 0;
   border-radius: 50%;
-  width: 180px;
-  height: 180px;
 `;
