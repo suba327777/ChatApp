543cba3009423c0e98337e56e5663b4218fa149c
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-08 20:39:20 +0900

    reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+/* paclages */
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+/* components */
 import { ChatPage } from "./components/pages/ChatPage";
 
-export const App = () => <ChatPage />;
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <ChatPage />;
+    </>
+  );
+};
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+`;
