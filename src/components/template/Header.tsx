af0920043bd6e0ae0a396a7b6ab08ea042df6d20
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-10 18:11:04 +0900

    css デザイン

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,3 +1,29 @@
+/* packages */
 import React from "react";
+import styled from "styled-components";
 
-export const Header = () => <div>Header</div>;
+export const Header: React.VFC = () => (
+  <SHeaderContainer>
+    <STitle>Realtime Chat</STitle>
+    <SSubTitle>React / React Redux / Firebase Realtime Database</SSubTitle>
+  </SHeaderContainer>
+);
+
+const SHeaderContainer = styled.div`
+  background-color: #222a41;
+  text-align: center;
+  color: white;
+  position: relative;
+`;
+
+const STitle = styled.h1`
+  font-size: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const SSubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: normal;
+  padding-bottom: 20px;
+`;
