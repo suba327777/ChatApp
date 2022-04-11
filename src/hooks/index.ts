c93ef07b6f7baa06d0b5eaae240a04050ed837a5
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-11 19:46:06 +0900

    first commit

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useAppDispatch } from "./useAppDispatch";
+export { useAppSelector } from "./useAppselector";
+export { useFetchData } from "./useFetchData";
+export { useFetchImage } from "./useFetchImage";
---
+++ src/store/slices/index.ts
@@ -0,0 +1,5 @@
+export { useUserIdDataSelector, setUserIdData } from "./user/userIdDataSlice";
+export { useUserNameDataSelector, setUserNameData } from "./user/userNameDataSlice";
+export { useChatDataSelector, setChatData } from "./chatDataSlice";
+export { useImageUrlDataSelector, setImageUrlData } from "./imageUrlDataSlice";
+export { useIsChatCompletedSelector, setIsChatCompleted } from "./isChatCompleted";
