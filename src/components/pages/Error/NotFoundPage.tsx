d7e11735aa439367fc08e5565ff34d6d9f517049
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-11 17:00:14 +0900

    css デザイン

--- src/components/pages/Error/NotFoundPage.tsx
+++ src/components/pages/Error/NotFoundPage.tsx
@@ -1,10 +1,23 @@
 /* packages */
 import React from "react";
-/* store */
+import styled from "styled-components";
 
 export const NotFoundPage: React.VFC = () => (
-  <div>
-    <h1>404</h1>
-    <h3>お探しのページは見つかりませんでした</h3>
-  </div>
+  <SContainer>
+    <STitle>404</STitle>
+    <SSubTitle>お探しのページは見つかりませんでした</SSubTitle>
+  </SContainer>
 );
+
+const SContainer = styled.div`
+  text-align: center;
+  padding-top: 35%;
+`;
+
+const STitle = styled.h1`
+  font-size: 40px;
+`;
+
+const SSubTitle = styled.h2`
+  font-size: 30px;
+`;
