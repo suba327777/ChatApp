ffae59b84e7092255cad70d2c65169d0c5d5f3a3
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-11 16:59:53 +0900

    first commit

---
+++ src/components/atoms/input/UserNameInput.tsx
@@ -0,0 +1,21 @@
+/* packages */
+/* packages */
+import React, { ChangeEvent } from "react";
+import { Input } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+type Props = {
+  placeholder: string;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CustomInput = styled(Input)({
+  width: "60%",
+  fontSize: "15px",
+  margin: "0 20px",
+});
+
+export const UserNameInput: React.VFC<Props> = ({ placeholder, value, onChange }) => (
+  <CustomInput placeholder={placeholder} value={value} onChange={onChange} />
+);
